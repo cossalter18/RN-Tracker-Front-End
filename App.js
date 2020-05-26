77714f11dfdc255924a1bcef52a69d2faaa853d4
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-25 21:16:30 -0500

    navigation track built

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreate from "./src/screens/TrackCreate";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackList from "./src/screens/TrackList";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackList,
+      TrackDetail: TrackDetailScreen,
+    }),
+    TrackCreate: TrackCreate,
+    Account: AccountScreen,
+  }),
 });
+
+
+export default createAppContainer(switchNavigator);
---
+++ src/screens/TrackCreate.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const TrackCreate = () => {
+  return <Text style={{ fontSize: 48 }}>TrackCreate</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackCreate;
