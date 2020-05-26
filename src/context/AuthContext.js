27acdca83b7c23f2f710bacc60d0d446834fe369
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-26 00:45:16 -0500

    sign in/sign up created and working

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,32 +1,29 @@
 import { AsyncStorage } from "react-native";
 import createDataContext from "./createDataContext";
 import tracker from "../api/tracker";
-import {navigate} from '../navigationRef'
+import { navigate } from "../navigationRef";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.payload };
-      case 'signup':
-          return { errorMessage: '' , token: action.payload}
+    case "signin":
+      return { errorMessage: "", token: action.payload };
     default:
       return state;
   }
 }; //end authReducer
 
-
 //Example
 //const add = (a,b) => a + b;
 
-
-
-const signup = dispatch => async ({ email, password }) => {
+const signup = (dispatch) => async ({ email, password }) => {
   //make api request to sign up with email and password
   try {
     const response = await tracker.post("/signup", { email, password });
-    await AsyncStorage.setItem('token', response.data.token);
-    dispatch({ type: 'signup', payload: response.data.token });
-    navigate('TrackList')
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+    navigate("TrackList");
   } catch (err) {
     dispatch({
       type: "add_error",
@@ -36,14 +33,23 @@ const signup = dispatch => async ({ email, password }) => {
   //if we signup, we want to modify our state, and say that we are authenticated
   //if signing up fails, we need to show error message
 };
- // end signup
+// end signup
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
-    //try to sign in
-    //handle success and update state
-    // handle failure by showing error message
-  };
+const signin = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await tracker.post("/signin", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch ({type: 'signin', payload: response.data.token})
+    navigate('TrackList')
+  } catch (err) {
+    dispatch({
+      type: "add_error",
+      payload: "Error with sign in",
+    });
+  }
+  //try to sign in
+  //handle success and update state
+  // handle failure by showing error message
 }; // end signing
 
 const signout = (dispatch) => {
@@ -55,5 +61,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  {token: null, errorMessage: ''}
+  { token: null, errorMessage: "" }
 );
